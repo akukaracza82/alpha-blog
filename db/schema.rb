3a837d48cf68023a42e8f594a3d7d9ebd5294c8d
Artur Mrozinski <amrozinski@gmail.com>
2020-05-24 18:01:07 +0100

    migration commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_164954) do
+ActiveRecord::Schema.define(version: 2020_05_17_172522) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "descriptions", force: :cascade do |t|
+    t.text "description"
   end
 
 end
